e8cec72b7ccecb532a9c9336ebb47223dded8bd8
huiralb <huiralb@gmail.com>
2015-11-04 12:59:20 +0700

    ganti path lokasi view/welcome_message.php & controller/Selamat_datang.php

--- resources/views/welcome_message.php
+++ resources/views/welcome_message.php
@@ -74,10 +74,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
 		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+		<code>resources/views/welcome_message.php</code>
 
 		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
+		<code>app/controllers/Selamat_datang.php</code>
 
 		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
 	</div>
