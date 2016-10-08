41a7587218e26164e09caea6aa79fe937c414e4d
huiralb <huiralb@gmail.com>
2016-10-08 23:26:44 +0700

    add backend module implement from HMVC

--- app/modules/backend/controllers/Backend.php
+++ app/modules/backend/controllers/Backend.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Base extends MX_Controller {
+class Backend extends MX_Controller {
 
 	public function __construct()
 	{
